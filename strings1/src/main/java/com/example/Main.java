3daa7c2e3911c2b115e6cd541398389be85fc7ed
micserv010821 <dotnet290320@gmail.com>
2023-03-02 17:38:07 +0200

    upload

--- strings1/src/main/java/com/example/Main.java
+++ strings1/src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static Person p3 = new Person();
+
+    static void foo(Person p1, int x) {
+        p1.x = 100;
+        p1 = null;
+        Person p2 = new Person();
+        x = 20;
+        String s1 = "hello";
+        String s2 = "hello";
+        if (s1 == s2) {
+         // wrong
+        }
+        if (s1 == s2 || s1.equals(s2)) {
+            // correct
+        }
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.x = 200;
+        int x = 12;
+        foo(p1, x);
     }
 }
---
+++ strings1/src/main/java/com/example/Person.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Person {
+
+    protected int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
